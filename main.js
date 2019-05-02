403f68fe078188cc5518d67f60ec94ab53cca462
Pol <polbene95@gmail.com>
2019-05-02 21:40:34 +0200

    Deploy button to test

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
-     var app = new Vue({
-                el: '#app',
-                data: {
-                message: 'Hello Vue!'
-                }
-            })
-     
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+    },
+    methods: {
+        notifyMe() {
+
+            let notification = new Notification('TITLE OF NOTIFICATION', {
+                icon: 'http://carnes.cc/jsnuggets_avatar.jpg',
+                body: "Hey! You are on notice!",
+                vibrate: [100, 50, 100]
+
+            });
+
+            notification.onclick = function () {
+                window.open("http://carnes.cc");
+            };
+        },
+        allowNotifications() {
+            Notification.requestPermission();
+        },
+        denyNotifications() {
+            Notification.permission = 'denied';
+        }
+    },
+    computed: {
+        permission() {
+            return Notification.permission
+        }
+    }
+})
+
 
 // JS Nuggets: Notifications API
 
@@ -13,31 +39,42 @@
 //new Notification("Subscribe to JS Nuggets!");
 
 function notifyMe() {
-  if (!("Notification" in window)) {
-    alert("This browser does not support system notifications");
-  }
-  else if (Notification.permission === "granted") {
-    notify();
-  }
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      if (permission === "granted") {
+    if (!("Notification" in window)) {
+        alert("This browser does not support system notifications");
+    } else if (Notification.permission === "granted") {
         notify();
-      }
-    });
-  }
-  
-  function notify() {
-    var notification = new Notification('TITLE OF NOTIFICATION', {
-      icon: 'http://carnes.cc/jsnuggets_avatar.jpg',
-      body: "Hey! You are on notice!",
+    } else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function (permission) {
+            if (permission === "granted") {
+                notify();
+            }
+        });
+    }
+
+}
+
+function notify() {
+    let notification = new Notification('TITLE OF NOTIFICATION', {
+        icon: 'http://carnes.cc/jsnuggets_avatar.jpg',
+        body: "Hey! You are on notice!",
+        vibrate: [100, 50, 100]
+
     });
 
     notification.onclick = function () {
-      window.open("http://carnes.cc");      
+        window.open("http://carnes.cc");
     };
-    setTimeout(notification.close.bind(notification), 7000); 
-  }
-
+    //    setTimeout(notification.close.bind(notification), 7000); 
 }
-notifyMe();
+//notifyMe();
+
+
+//window.addEventListener('load', () => {
+//  navigator.serviceWorker.register('/worker.js')
+//  .then((registration) => {
+//    console.log('Service Worker registration completed with scope: ',
+//      registration.scope)
+//  }, (err) => {
+//    console.log('Service Worker registration failed', err)
+//  })
+//})
